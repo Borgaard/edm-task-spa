66689f0b0116b8628c0275c5bf9daa9b2ce0c2d5
nicoleborgaard@gmail.com <nicoleborgaard@gmail.com>
2015-02-17 21:52:18 -0800

    Input quad 0, next is saving data

--- app/scripts/controllers/main.js
+++ app/scripts/controllers/main.js
@@ -8,22 +8,41 @@
  * Controller of the queueSpangularApp
  */
 angular.module('queueSpangularApp')
-  .controller('TaskCtrl', function ($scope) {
-    //each quadrant: 0, 1, 2, 3
-  //   $scope.impUrgent      = [{text: 'I am impUrgent'}];
-  //   $scope.impNotUrgent   = [{text: 'I am impNotUrgent'}];
-  //   $scope.unimpUrgent    = [{text: 'I am unimpUrgent'}];
-  //   $scope.unimpNotUrgent = [{text: 'I am unimpNotUrgent'}];
-  // })
+    .controller('impurgCtrl', ['$scope', function ($scope) {
+  	$scope.impurg = '';
+  	$scope.task = function() {
+  		$scope.taskStatus = 'Your important and urgent task is: ' + $scope.impurg + '!';
+  		$scope.impurg = '';
+  	};
+  }])
+
+  .controller('impnouCtrl', ['$scope', function ($scope) {
+  	var impnou = this;
+  	uniurg.task = [];
+  }])
+
+  .controller('uniurgCtrl', ['$scope', function ($scope) {
+  	var uniurg = this;
+  	uniurg.task = [];
+  }])
+
+  .controller('uninouCtrl', ['$scope', function ($scope) {
+  	var uninou = this;
+  	uniurg.task = [];
+  }]);
 
   // .controller('TaskCtrl', function ($scope) {
-  	$scope.savedTask = null; //validate this in HTML
-  	$scope.save = function(task) {
-  		$scope.savedTask = angular.copy(task);
-  	};
-  });
+  //   //each quadrant: 0, 1, 2, 3
+  // //   $scope.impUrgent      = [{text: 'I am impUrgent'}];
+  // //   $scope.impNotUrgent   = [{text: 'I am impNotUrgent'}];
+  // //   $scope.unimpUrgent    = [{text: 'I am unimpUrgent'}];
+  // //   $scope.unimpNotUrgent = [{text: 'I am unimpNotUrgent'}];
+  // // })
 
-
-
-
+  // // .controller('TaskCtrl', function ($scope) {
+  // 	$scope.savedTask = null; //validate this in HTML
+  // 	$scope.save = function(task) {
+  // 		$scope.savedTask = angular.copy(task);
+  // 	};
+  // })
 
