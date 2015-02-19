82441e3d9b09d293935cc152b2e02294a132c211
nicoleborgaard@gmail.com <nicoleborgaard@gmail.com>
2015-02-18 23:02:18 -0800

    Yaaay service dependency issue fixed.

--- app/scripts/controllers/main.js
+++ app/scripts/controllers/main.js
@@ -8,9 +8,19 @@
  * Controller of the queueSpangularApp
  */
 angular.module('queueSpangularApp')
-		.controller('allTaskCtrl', function ($scope, Task) {
-			$scope.tasks = Task;
-		})
+		// .controller('allTaskCtrl', ['$scope', '$firebase', 'Task', function ($scope, $firebase, Task) {
+		// 	var ref = new Firebase('https://visitask.firebaseio.com/data');
+		// 	//AngularFire reference to Firebase data
+		// 	var sync = $firebase(ref);
+		// 	//download ze data into local object
+		// 	var syncObject = sync.$asObject();
+		// 	// $scope.data = sync.$asObject(); //above replaces this as three-way data binding
+		// 	//sync object with three-way data binding JS, DOM and Firebase together
+		// 	//$asObject creates sync object, then $bindTo() binds to $scope variable
+		// 	syncObject.$bindTo($scope, 'data');
+
+		// 	$scope.tasks = Task;
+		// }])
 
     .controller('impurgCtrl', ['$scope', 'Task', function ($scope, Task) {
     	$scope.newTask = '';
@@ -43,10 +53,11 @@ angular.module('queueSpangularApp')
       $scope.newTask = '';
   		$scope.uninou = Task.uninou;
   		$scope.task = function() {
-  		$scope.uninou.push({desc: $scope.newTask});
-  		$scope.newTask = '';
-  	};
-  }]);
+  			$scope.uninou.push({desc: $scope.newTask});
+  			$scope.newTask = '';
+  		};
+  	}
+  ]);
 
   // .controller('TaskCtrl', function ($scope) {
   //   //each quadrant: 0, 1, 2, 3
--- app/scripts/services/task.js
+++ app/scripts/services/task.js
@@ -2,22 +2,13 @@
 
 angular.module('queueSpangularApp')
 
-.factory('Task', ['scope', '$firebase', function ($scope, $firebase) {
-	var ref = new Firebase('https://visitask.firebaseio.com/data');
-	//AngularFire reference to Firebase data
-	var sync = $firebase(ref);
-	//download ze data into local object
-	$scope.data = sync.$asObject();
-	//sync object with three-way data binding JS, DOM and Firebase together
-	//$asObject creates sync object, then $bindTo() binds to $scope variable
-	//
-	syncObject.$bindTo($scope, 'data');
+.factory('Task', function () {
 	
-	var Task = {
+	var taskAllQuads = {
 				impurg: [],
 				impnou: [],
 				uniurg: [],
 				uninou: []
 		};
-	return Task;
-}]);
+	return taskAllQuads;
+});
